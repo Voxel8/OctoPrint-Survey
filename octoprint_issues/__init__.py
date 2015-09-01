be441ac1b3853d048dde0938b969291404ba51a0
Jack Minardi <jack@minardi.org>
2015-09-01 11:21:29 -0400

    initial implementation that opens a link on failure and completion

--- octoprint_issues/__init__.py
+++ octoprint_issues/__init__.py
@@ -1,30 +1,37 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
-class IssuesPlugin(octoprint.plugin.TemplatePlugin):
-	# TODO Implement me!
-	pass
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Issues Plugin"
+class IssuesPlugin(octoprint.plugin.EventHandlerPlugin, octoprint.plugin.AssetPlugin):
+
+    def get_update_information(self, *args, **kwargs):
+        return dict(
+            alignment_plugin=dict(
+                type="github_commit",
+                user="Voxel8",
+                repo="OctoPrint-Issues",
+                branch='master',
+                pip="https://github.com/Voxel8/OctoPrint-Issues/archive/{target_version}.zip",
+            )
+        )
+
+    def get_assets(self):
+         return {
+             "js": ["js/issues.js"],
+             #"css": ["css/navbartemp.css"],
+             #"less": ["less/navbartemp.less"]
+         }
+
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = IssuesPlugin()
+    global __plugin_hooks__
+    global __plugin_implementation__
 
-	# global __plugin_hooks__
-	# __plugin_hooks__ = {
-	#    "some.octoprint.hook": __plugin_implementation__.some_hook_handler
-	# }
+    plugin = IssuesPlugin()
 
+    __plugin_implementation__ = plugin
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": plugin.get_update_information,
+    }
---
+++ octoprint_issues/static/js/issues.js
@@ -0,0 +1,37 @@
+$(function() {
+    function IssuesViewModel(parameters) {
+        var self = this;
+
+        self.appearance = parameters[0];
+
+        self.onBeforeBinding = function() {
+            console.log("FOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+        };
+
+        self.onEventPrintFailed = function(payload) {
+            var params = self.encodeQueryData({'title': 'PrintFailed'});
+            var url = 'https://github.com/Voxel8/DevKit-Issues/issues/new?' + params;
+            window.open(url);
+        };
+
+        self.onEventPrintDone = function(payload) {
+            var params = self.encodeQueryData({'title': 'PrintDone'});
+            var url = 'https://github.com/Voxel8/DevKit-Issues/issues/new?' + params;
+            window.open(url);
+        };
+
+        self.encodeQueryData= function(data) {
+            var ret = [];
+            for (var d in data)
+                ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+            return ret.join("&");
+        };
+
+    }
+
+    OCTOPRINT_VIEWMODELS.push([
+        IssuesViewModel,
+        ["appearanceViewModel"],
+        ""
+    ]);
+});
