8346b0b02ddcb8f8595240b8ba57f755b3df83e7
Jack Minardi <jack@minardi.org>
2015-09-07 18:39:17 -0400

    submit printer name and filename

--- octoprint_issues/static/js/issues.js
+++ octoprint_issues/static/js/issues.js
@@ -3,15 +3,22 @@ $(function() {
         var self = this;
 
         self.appearance = parameters[0];
+        self.profiles = parameters[1];
         self.url = 'https://github.com/Voxel8/Print-Reporting/issues/new?';
 
         self.onEventPrintFailed = function(payload) {
-            var params = self.encodeQueryData({'title': 'PrintFailed'});
+            var params = self.encodeQueryData(
+                {'title': self.profiles.currentProfile.name + ' PrintFailed: ' + payload.file}
+            );
+            console.log(self.profiles);
             window.open(self.url + params);
         };
 
         self.onEventPrintDone = function(payload) {
-            var params = self.encodeQueryData({'title': 'PrintDone'});
+            var params = self.encodeQueryData(
+                {'title': self.profiles.currentProfile.name + ' PrintDone: ' + payload.filename}
+            );
+            console.log(payload);
             window.open(self.url + params);
         };
 
@@ -26,7 +33,7 @@ $(function() {
 
     OCTOPRINT_VIEWMODELS.push([
         IssuesViewModel,
-        ["appearanceViewModel"],
+        ["appearanceViewModel", "printerProfilesViewModel"],
         ""
     ]);
 });
