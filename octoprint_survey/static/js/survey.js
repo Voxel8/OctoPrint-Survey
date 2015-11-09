60dd7ef5059fcab06e4d0a6fd93ece2351927077
Jack Minardi <jack@minardi.org>
2015-11-08 22:11:24 -0800

    Update to new form URLs

--- octoprint_survey/static/js/survey.js
+++ octoprint_survey/static/js/survey.js
@@ -5,42 +5,40 @@ $(function() {
         self.appearance = parameters[0];
         self.profiles = parameters[1];
 
-        self.printUrl = 'https://docs.google.com/a/voxel8.co/forms/d/1kF5Bdiu50WfGmzpM2lSvXbDEZ6_k4ka1wx5i-30XFV8/viewform?embedded=true&';
-        self.feedbackUrl = 'https://docs.google.com/a/voxel8.co/forms/d/1kbKEUF1HuKT6Efr-kJBQoHkuFX2srVt68mZ7JbCqXEs/viewform?embedded=true&';
+        self.printUrl = 'https://docs.google.com/forms/d/1blQESehguSvkYH9VAJFku2xKCqJn9UnMRqMWEuwhABE/viewform?embedded=true&';
+        self.feedbackUrl = 'https://formcrafts.com/a/15350';
 
         self.feedbackLink = document.getElementById('feedback');
 
         self.feedbackLink.onclick = function() {
             $.get('/api/plugin/survey', success=function(data){
-                var loglines = data.loglines;
-                console.log(loglines);
-                var params = self.encodeQueryData({
-                    'entry.1274091219': loglines,
-                });
+                //var loglines = data.loglines;
+                //console.log(loglines);
+                //var params = self.encodeQueryData({
+                //    'entry.1274091219': loglines,
+                //});
                 $.featherlight({
-                    iframe: self.feedbackUrl + params,
-                    iframeWidth: 800,
-                    iframeHeight: 600
+                    iframe: self.feedbackUrl,// + params,
+                    iframeWidth: 600,
+                    iframeHeight: 800
                 });
             });
         };
 
         self.onEventPrintFailed = function(payload) {
             var params = self.encodeQueryData({
-                'entry.1137737192': 'PrintFailed',  //Action
-                'entry.489248788': payload.file,  //Filename
+                'entry.1092334781': payload.file,  //Filename
             });
             $.featherlight({
                 iframe: self.printUrl + params,
                 iframeWidth: 800,
-                iframeHeight: 600
+                iframeHeight: 800
             });
         };
 
         self.onEventPrintDone = function(payload) {
             var params = self.encodeQueryData({
-                'entry.1137737192': 'PrintDone',  //Action
-                'entry.489248788': payload.filename,  //Filename
+                'entry.1092334781': payload.filename,  //Filename
             });
             $.featherlight({
                 iframe: self.printUrl + params,
